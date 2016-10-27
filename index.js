9583d40045bd66e416da6e920d930bb7d1a82eef
John Nowinsky <armthepit@armthepit.com>
2016-10-27 15:35:59 -0500

    add function to ask for product to purchase and skeleton function for quantity to purchase.

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var mysql = require('mysql');
 var inquirer = require('inquirer');
 var clear = require('clear');
 var tab = require('table-master');
+
 var connection = mysql.createConnection({
     host: "localhost",
     port: "3306",
@@ -10,23 +11,21 @@ var connection = mysql.createConnection({
     database: "bamazondb"
 })
 
-var customer = 0;
-
 //Connect To Bamazon database
 function dbConnect() {
-	connection.connect(function(error) {
-	    if (error) throw error;
-	    // clear();
-	    console.log(connection.threadId);
-	    customer = connection.threadId;
-	    productList();
-	})	
+    connection.connect(function(error) {
+        if (error) throw error;
+        // clear();
+        console.log(connection.threadId);
+        customer = connection.threadId;
+        productList();
+    })  
 }
 
 //Build a table of products for sale
 function productList() {
     clear();
-    console.log('Serving customer ' + customer + '\n');	
+    console.log('Serving customer ' + customer + '\n'); 
     connection.query('SELECT * FROM products', function(error, result) {
         var choiceArray = [];
         for (var i = 0; i < result.length; i++) {
@@ -62,72 +61,14 @@ function placeOrder() {
     })
 };
 
-
 //Ask for quantity to purchase
 function purchaseQuantity(itemid, productName, price, stockQuantity) {
-    inquirer.prompt({
-        name: "quantity",
-        type: "input",
-        message: "\n How many would you like to purchase?"        
-    }).then(function(quantity) {
-    	var purchaseQuantity = parseInt(quantity.quantity);
-        if (purchaseQuantity > 0) {
-        	inventoryCheck(itemid, productName, price, stockQuantity, purchaseQuantity);
-        } else {
-	        console.log("\n Please check the quantity to purchase.");
-	        purchaseQuantity(itemid, productName, price, stockQuantity);
-	        // placeOrder();
-        }
-    })
+
 }
 
-//Check inventory to see if order can be filled
-function inventoryCheck(itemid, productName, price, stockQuantity, purchaseQuantity) {
-    if (purchaseQuantity <= stockQuantity) {
-        updateInventory(itemid, productName, price, stockQuantity, purchaseQuantity);
-    } else {
-        console.log("\n We are unable to fill your order for " + productName + ". There's only " + stockQuantity + " available. Please update quantity.");
-        placeOrder();
-    }
-}
 
-//Update inventory after sale
-function updateInventory(itemid, productName, price, stockQuantity, purchaseQuantity) {
-    stockQuantity -= purchaseQuantity;
-    var query = 'UPDATE products SET stockquantity = ? WHERE itemid = ?';
-    connection.query(query, [stockQuantity, itemid], function(error, result) {
-        if (error) throw error;
-        completeOrder(itemid, productName, price, stockQuantity, purchaseQuantity);
-    });
-}
+function runMainMenu() {
 
-//Sale summary
-function completeOrder(itemid, productName, price, stockQuantity, purchaseQuantity) {
-    console.log('\nThank you Customer ' + customer + ' for your order.');
-    // console.log(purchaseQuantity);
-    console.log( "You are purchasing " + purchaseQuantity + ' ' + productName + " at $" + price.toFixed(2) + ' each.');
-    console.log( "Your total is $"  + price * purchaseQuantity.toFixed(2)); 
-    continueShopping();
-}
-
-//Ask if customer wants to keep shopping.
-function continueShopping() {
-    inquirer.prompt({
-        name: "answer",
-        type: "input",
-        message: "\n Do you need anything else (y/n)?"
-    }).then(function(answer) {
-    	var answer = answer.answer.toLowerCase();
-        if (answer == 'y') {
-        	console.log("\n");
-			productList();
-        } else {
-        	console.log("\nThank you again for dropping by.");
-        	connection.end();
-        }
-    })	
 }
 
 dbConnect();
-
-
